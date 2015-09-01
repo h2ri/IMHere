f4bf4d3e7171a8a1c8ed124fb564308afaff1c1e
Hari <ha219ri@gmail.com>
2015-09-01 12:07:18 +0530

    login response update

--- users/serializers.py
+++ users/serializers.py
@@ -1,41 +1,44 @@
-from django.contrib.auth.models import User,Group
-from models import MyUser
+from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from .models import MyUser
 from oauth2_provider.models import Application, AccessToken, RefreshToken
-from datetime import datetime
-import httplib
-import urllib
+
 from django.utils import timezone
-# first we define the serializers
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         write_only_fields = ('password',)
 
+
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
 
+
 class SignUpSerializer(serializers.ModelSerializer):
     client_id = serializers.SerializerMethodField()
     client_secret = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ('username','password','client_id','client_secret')
-        #write_only_fields = ('password',)
+        fields = ('username', 'password', 'client_id', 'client_secret')
+        # write_only_fields = ('password',)
+
     def get_client_id(self, obj):
         return Application.objects.get(user=obj).client_id
+
     def get_client_secret(self, obj):
         return Application.objects.get(user=obj).client_secret
+
     def create(self, validated_data):
-            password = validated_data.pop('password', None)
-            instance = self.Meta.model(**validated_data)
-            if password is not None:
-                instance.set_password(password)
-                instance.save()
-            return instance
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+            instance.save()
+        return instance
+
     def update(self, instance, validated_data):
         for attr, value in validated_data.items():
             if attr == 'password':
@@ -45,6 +48,7 @@ class SignUpSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
 
+
 class LoginSerializer(SignUpSerializer):
     access_token = serializers.SerializerMethodField()
     refresh_token = serializers.SerializerMethodField()
@@ -53,23 +57,24 @@ class LoginSerializer(SignUpSerializer):
     client_secret = serializers.SerializerMethodField()
     token_flag = serializers.SerializerMethodField()
     flag = 0
+
     class Meta:
         model = User
-        fields = ('client_id','client_secret','access_token','refresh_token','expires','token_flag')
-    def get_access_token(self,obj):
+        fields = ('client_id', 'client_secret', 'access_token', 'refresh_token', 'expires', 'token_flag')
+
+    def get_access_token(self, obj):
         return AccessToken.objects.get(user=obj).token
-        
-    def get_refresh_token(self,obj):
+
+    def get_refresh_token(self, obj):
         return RefreshToken.objects.get(user=obj).token
-        
-    def get_expires(self,obj):       
+
+    def get_expires(self, obj):
         Comparetime = AccessToken.objects.get(user=obj).expires
         if Comparetime > timezone.now():
             LoginSerializer.flag = 0
         else:
             LoginSerializer.flag = 1
         return Comparetime
-    
-    def get_token_flag(self,obj):
+
+    def get_token_flag(self, obj):
         return LoginSerializer.flag
-    
--- users/views.py
+++ users/views.py
@@ -1,50 +1,56 @@
+import httplib
+import urllib
+import json
+
 from django.contrib.auth.models import User, Group
 from rest_framework import permissions, viewsets, generics
 from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope, TokenHasScope
-from .serializers import UserSerializer, GroupSerializer, SignUpSerializer, LoginSerializer
-from oauth2_provider.decorators import protected_resource
-from .permissions import IsAuthenticatedOrCreate
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.response import Response
-from rest_framework.generics import CreateAPIView
+
 from rest_framework import status
-from oauth2_provider.models import Application, AccessToken, RefreshToken
-import httplib
-import base64
-import urllib
-import json
-from rest_framework.serializers import Serializer
+
+from rest_framework.renderers import JSONRenderer
+
+from oauth2_provider.models import AccessToken
+
+from .serializers import UserSerializer, GroupSerializer, SignUpSerializer, LoginSerializer
+from .permissions import IsAuthenticatedOrCreate
+
 
 class UserViewSet(viewsets.ModelViewSet):
-
     permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     queryset = User.objects.all()
-    #permission_classes = (permissions.AllowAny,)
-    
+    # permission_classes = (permissions.AllowAny,)
+
     serializer_class = UserSerializer
+
     def get_queryset(self):
         if self.request.user.is_superuser:
             return User.objects.all()
         else:
             return User.objects.filter(id=self.request.user.id)
-        
-def getAuthToken(creds,password):
+
+
+def getAuthToken(creds, password):
     conn = httplib.HTTPConnection("127.0.0.1:8000")
-    url =  "/o/token/"
+    url = "/o/token/"
     headersMap = {
-    "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Type": "application/x-www-form-urlencoded",
     };
-    data = {'username':str(creds["username"]),'password':str(password),'grant_type':str('password'),'client_id':str(creds["client_id"]),'client_secret':str(creds["client_secret"])
-    }
+    data = {'username': str(creds["username"]), 'password': str(password), 'grant_type': str('password'),
+            'client_id': str(creds["client_id"]), 'client_secret': str(creds["client_secret"])
+            }
     requestUrl = url + "?" + urllib.urlencode(data)
     conn.request("POST", requestUrl, headers=headersMap)
     response = conn.getresponse()
     if response.status == 200:
         data = response.read()
-        result = json.loads( data )
+        result = json.loads(data)
         return result
     else:
-    	return response
+        return response
+
 
 class GroupViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
@@ -52,65 +58,70 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
+
 class SignUp(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = SignUpSerializer
     permission_classes = (IsAuthenticatedOrCreate,)
-    
-    
-    def create(self,request, *args, **kwargs):
-        password = request.POST.get('password','')
+
+    def create(self, request, *args, **kwargs):
+        password = request.POST.get('password', '')
         serializer = self.get_serializer(data=request.DATA)
-        
+
         if serializer.is_valid():
             self.object = serializer.save()
             headers = self.get_success_headers(serializer.data)
             creds = serializer.data
-            #calling for accesstoken
-            token =  getAuthToken(creds,password)
-            q = User.objects.get(username = creds["username"])
+            # calling for accesstoken
+            token = getAuthToken(creds, password)
+            q = User.objects.get(username=creds["username"])
             p = AccessToken.objects.get(user=q.id)
-            #print p.token
-            #print p.expires
-            #print RefreshToken.objects.get(user=q.id).token
-            return Response(token ,status=status.HTTP_201_CREATED,
+            # print p.token
+            # print p.expires
+            # print RefreshToken.objects.get(user=q.id).token
+            return Response(token, status=status.HTTP_201_CREATED,
                             headers=headers)
 
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#Login
-#username and password
-#return ClientID ClientSecret AccessToken RefereshToken ExpireTime TokenFlag
+
+# Login
+# username and password
+# return ClientID ClientSecret AccessToken RefereshToken ExpireTime TokenFlag
 class Login(generics.ListAPIView):
-    #queryset = User.objects.all()
+    # queryset = User.objects.all()
     serializer_class = LoginSerializer
     authentication_classes = (BasicAuthentication,)
+
     def get_queryset(self):
         return [self.request.user]
-    
+
     def list(self, request, *args, **kwargs):
         response = generics.ListAPIView.list(self, request, *args, **kwargs)
         data1 = response.data
         if data1[0]['token_flag'] == 1:
             if getAuthFromRefreshToken(data1[0]):
-                return generics.ListAPIView.list(self, request, *args, **kwargs)
+                data2 = generics.ListAPIView.list(self, request, *args, **kwargs).data
+                data2[0]['refresh_token'] = " "
+                return Response(data2, status=status.HTTP_200_OK)
         else:
-            return response
+            data1[0]['refresh_token'] = " "
+            return Response(data1, status=status.HTTP_201_CREATED)
 
 
 # To Obtain Refresh Token if Access Token Expired 
-# /o/token for refresh  
+# /o/token for refresh
 def getAuthFromRefreshToken(credtoken):
-    #print credtoken['access_token']
+    # print credtoken['access_token']
     conn = httplib.HTTPConnection("127.0.0.1:8000")
-    url =  "/o/token/"
+    url = "/o/token/"
     headersMap = {"Content-Type": "application/x-www-form-urlencoded",
-    };
-    data = {'refresh_token':str(credtoken['refresh_token']),'grant_type':str('refresh_token'),'client_id':str(credtoken['client_id']),'client_secret':str(credtoken['client_secret'])
-    }
+                  };
+    data = {'refresh_token': str(credtoken['refresh_token']), 'grant_type': str('refresh_token'),
+            'client_id': str(credtoken['client_id']), 'client_secret': str(credtoken['client_secret'])
+            }
     requestUrl = url + "?" + urllib.urlencode(data)
     conn.request("POST", requestUrl, headers=headersMap)
     response = conn.getresponse()
     if response.status == 200:
         return 1
-    
